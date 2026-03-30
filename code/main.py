7672ac8afbb25d8c553cf11d60d7aa6b7cb30175
London <vh186731@gmail.com>
2026-03-30 08:13:46 +0300

    First HTTP request

--- code/main.py
+++ code/main.py
@@ -1,3 +1,17 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+import socket
+ip = "192.168.1.8"
+def check_port(ip, port):
+    try:
+        s = socket.socket()
+        s.settimeout(2)
+        s.connect((ip, port))
+        request = "GET / HTTP/1.1\r\nHost: " + ip + "\r\n\r\n"
+        s.send(request.encode())
+        response = s.recv(4096)
+        print(response.decode(errors="ignore"))
+        s.close()
+        return True
+    except:
+        return False
+
+check_port(ip, 80)
